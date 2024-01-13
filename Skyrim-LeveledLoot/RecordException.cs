d4b96e7be914f9882063e9238a0af5f43b1fac49
Jampi0n <27952849+Jampi0n@users.noreply.github.com>
2024-01-13 10:51:46 +0100

    Update RecordException.cs

--- Skyrim-LeveledLoot/RecordException.cs
+++ Skyrim-LeveledLoot/RecordException.cs
@@ -5,8 +5,6 @@ using Mutagen.Bethesda.Plugins;
 namespace LeveledLoot {
     class RecordException : Exception {
 
-        string recordDescription;
-
         public RecordException(IFormLinkGetter record, string message) : base("[" + record.FormKey + "~?]: " + message) { }
 
         public RecordException(IMajorRecordGetter record, string message) : base("[" + record.FormKey + "~" + record.EditorID + "]: " + message) { }
